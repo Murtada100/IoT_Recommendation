3373e6c095d55198d76bb4ff68619c69233fbc29
Murtada Dohan <mortadaabbas31@yahoo.com>
2020-09-30 12:54:27 +0100

    2nd

--- html/treelist.js
+++ html/treelist.js
@@ -1,17 +1,9 @@
-$(document).ready(function(){
-
-    // Le left-menu sign
-    /* for older jquery version
-    $('#left ul.nav li.parent > a > span.sign').click(function () {
-        $(this).find('i:first').toggleClass("icon-minus");
-    }); */
-    
-    $(document).on("click","#left ul.nav li.parent > a > span.sign", function(){          
-        $(this).find('i:first').toggleClass("icon-minus");      
-    }); 
-    
+ 
+$(document).on("click","#left ul.nav li.parent > a > span.sign", function(){          
+    $(this).find('i:first').toggleClass("icon-minus");      
+}); 
     // Open Le current menu
-    $("#left ul.nav li.parent.active > a > span.sign").find('i:first').addClass("icon-minus");
-    $("#left ul.nav li.current").parents('ul.children').addClass("in");
-
-});
+$("#left ul.nav li.parent.active > a > span.sign").find('i:first').addClass("icon-minus");
+$("#left ul.nav li.current").parents('ul.children').addClass("in");
+var cloud=[];
+ 
